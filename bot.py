b7d713cf22d48b6aedad613ca1590de58a17ae58
Sophia <44215812+SophFru@users.noreply.github.com>
2020-10-20 16:06:10 -0500

    Added base code
    Added the base code with no commands

---
+++ bot.py
@@ -0,0 +1,23 @@
+# bot.py
+import os
+import random
+#import discord
+
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send('You do not have the correct role for this command.')
+
+bot.run(TOKEN)
