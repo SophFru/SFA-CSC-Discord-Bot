a1de1bebb59f12168375215cfa7616700aa39006
Sophia <44215812+SophFru@users.noreply.github.com>
2020-10-21 18:12:31 -0500

    Removed on_command_error
    This event is unnecessary since all the features we have planned can be used by all memebers.

--- bot.py
+++ bot.py
@@ -15,11 +15,6 @@ bot = commands.Bot(command_prefix='!')
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.errors.CheckFailure):
-        await ctx.send('You do not have the correct role for this command.')
-
 @bot.command(name= 'ping', help='tests if bot is responding')
 async def pingpong(ctx):
 	await ctx.send('pong')
