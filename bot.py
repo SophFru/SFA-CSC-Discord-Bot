012516efd1635056266eace968a198653512c82f
Sophia <44215812+SophFru@users.noreply.github.com>
2020-10-22 17:51:28 -0500

    added intents = intents

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
