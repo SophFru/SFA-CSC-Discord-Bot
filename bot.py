f7ad3c4fdf4a3304992c8baf757a69e1ed0d461f
Carl Hawley <carlhawley@gmail.com>
2020-10-29 17:44:16 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 import os
 import random
 import discord
+import re
 
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -19,12 +20,42 @@ async def on_ready():
 async def pingpong(ctx):
 	await ctx.send('pong')
 
-@bot.command(name= 'new_color', help='adds hex code color to role list')
-async def new_color(ctx, color_name: str, color_req: discord.Color):
+@bot.command(name= 'color_me', help='adds user to role of hex code color')
+async def color_me(ctx, color_req: str):
+    try: 
+        color = discord.Color(value=int(color_req,16))
+    except:
+        await ctx.send(f'You\'re not allowed to be {color_req}.')
+        return
+    if color.value == 0:
+        await ctx.send('Invalid color.')
+        return
+
     guild = ctx.guild
-    existing_color = discord.utils.get(guild.roles, color=color_req)
+    member = ctx.message.author
+
+    print(f'Running color_me')
+    for role in member.roles:
+        print(f'{role.name}')
+        if re.match('#[\dabcdef]{6}', role.name):
+            print(f'Removing user from color role: {role.name}')
+            await member.remove_roles(role)
+    existing_color = discord.utils.get(guild.roles, color=color)
     if not existing_color:
-        print(f'Creating new Role: {color_req}')
-        await guild.create_role(reason='bot color add', name=color_name,color=color_req)
+        print(f'User created new color role: {color}')
+        await guild.create_role(reason='bot color add', name=str(color),color=color)
+        existing_color = discord.utils.get(guild.roles, color=color)
+    print(f'Adding user to color role: {color}')
+    await member.add_roles(existing_color)
+
+@bot.command(name = 'color_cleanup', help='removes empty colors from role list')
+async def color_cleanup(ctx):
+    guild = ctx.guild
+    print(f'Running color role cleanup')
+    for role in guild.roles:
+        if re.match('#[\dabcdef]{6}', role.name) and len(role.members) == 0:
+            print(f'Deleting {role.name}, members: {len(role.members)}')
+            await role.delete()
+    print(f'Cleanup done')
 
 bot.run(TOKEN)
